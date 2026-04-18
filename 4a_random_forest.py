61b919a79e8bceae506d2ea229f93e202302ab03
Chirag-Mokashi <57129710+Chirag-Mokashi@users.noreply.github.com>
2026-04-18 13:21:29 -0400

    chore(rf): bump to v2 and redirect to results/RF_v2 for post-fix re-run

--- 4a_random_forest.py
+++ 4a_random_forest.py
@@ -21,10 +21,10 @@ from utils.rf_svm_loader import get_fold_data
 
 # ---- Config ----
 MODEL          = 'RF'
-VERSION        = 'v1'
+VERSION        = 'v2'
 N_ESTIMATORS   = 500
 RANDOM_SEED    = 42
-RESULTS_DIR    = Path('results/RF')
+RESULTS_DIR    = Path('results/RF_v2')
 RESULTS_CSV    = RESULTS_DIR / 'rf_{}_results.csv'.format(VERSION)
 SUMMARY_CSV    = RESULTS_DIR / 'rf_{}_summary.csv'.format(VERSION)
 CSV_COLS       = ['model', 'method', 'n_bands', 'fold',
